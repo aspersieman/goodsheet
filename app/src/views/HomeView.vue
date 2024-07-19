1827f8b7297ec43339b9bcaf802f7a6c2aded36f
Nicol van der Merwe <nicol@jvrafrica.co.za>
2024-07-19 08:30:59 +0200

    Setup layout, theme

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl">
+      <section
+        class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+      </section>
+    </div>
   </main>
 </template>
